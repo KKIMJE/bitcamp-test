9ac009a8b1af79cf3e82c82c508a94f69f2d55fe
kinnatzzang <shinich13@naver.com>
2022-03-10 18:34:41 +0900

    변경완

--- app/src/main/java/com/bitcamptest/Hello2.java
+++ app/src/main/java/com/bitcamptest/Hello2.java
@@ -4,10 +4,10 @@ public class Hello2 {
 
   public static void main(String[] args) {
     System.out.println("소주마시자");
-   
+
     System.out.println("한상은이다");
-    
-    
+
+
   }
   static void jueun() { 
     System.out.println("주은주은");
@@ -22,12 +22,13 @@ public class Hello2 {
     System.out.println("hhahhahha");
     System.out.println("hhahhahha");
     System.out.println("hhahhahha");
-  
+
   }
 
   static void m1() {
     System.out.println("규범입니당");
     System.out.println("한상은인데요");
     System.out.println("성은임");
+    System.out.println("오늘 감튀에 느린마을 먹자!");
   }
 }
