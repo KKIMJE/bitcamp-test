d4beb195592bcff74c01c188d96100e06862aaee
ture9200 <didtjddms09@naver.com>
2022-03-10 17:56:13 +0900

    커밋 ㅎㅎ

--- app/src/main/java/com/bitcamptest/Hello2.java
+++ app/src/main/java/com/bitcamptest/Hello2.java
@@ -4,7 +4,7 @@ public class Hello2 {
 
   public static void main(String[] args) {
     System.out.println("소주마시자");
-    System.out.println("술술술수루술");
+    System.out.println("성은임");
     System.out.println("한상은이다");
   }
   static void jueun() { 
