3c4e6ba8fc29a75a9ca91070ca1b86289bbd86b2
Tissue94 <tissue2979@naver.com>
2022-03-10 17:46:45 +0900

    규범 편집

--- app/src/main/java/com/bitcamptest/Hello2.java
+++ app/src/main/java/com/bitcamptest/Hello2.java
@@ -4,5 +4,10 @@ public class Hello2 {
 
   public static void main(String[] args) {
     System.out.println("소주마시자");
+    m1();
+  }
+
+  static void m1() {
+    System.out.println("규범입니당");
   }
 }
